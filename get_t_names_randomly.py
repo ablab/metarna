5625b4cd84302d812687001a70dbcca72caa40ee
letovesnoi <lenk.ff@gmail.com>
2020-07-14 19:35:43 +0300

    Write chromosomes for selected transcripts

--- get_t_names_randomly.py
+++ get_t_names_randomly.py
@@ -10,11 +10,13 @@ t_needed = int(sys.argv[2])
 
 gene_set = set()
 transcript_set = set()
+chr_set = set()
 
 lines = open(gtf).read().splitlines()
 n_line = random.randint(50, len(lines) - t_needed * 30)
 for line in lines[n_line:]:
     fields = line.strip().split('\t')
+    chr = fields[0]
     type = fields[2]
     others = fields[8].split('; ')
     g_id = others[0].split('"')[1]
@@ -23,12 +25,14 @@ for line in lines[n_line:]:
         t_needed -= 1
         gene_set.add(g_id)
         transcript_set.add(t_id)
+        chr_set.add(chr)
     if t_needed == 0:
         break
 
 print('Line: {} / {}'.format(n_line, len(lines)))
 print('Genes: ' + ' '.join(gene_set))
 print('Transcripts: ' + ' '.join(transcript_set))
+print('chr: ' + ' '.join(chr_set))
 
 fout = open(sys.argv[3] + '.names', 'w')
 fout.write('\n'.join(transcript_set))
