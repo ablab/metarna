d4b295d4cf123bd8916fe0a5d20dce07d0e3853f
letovesnoi <lenk.ff@gmail.com>
2020-11-18 16:01:17 +0300

    Save learned *.isoforms.results file and only modify tpm

--- scratches/nanosim_to_rsem_abundances.py
+++ scratches/nanosim_to_rsem_abundances.py
@@ -1,24 +1,41 @@
-# python nanosim_to_rsem_abundances.py nanosim_abundance.tsv rsem_abundance.tsv
+# python nanosim_to_rsem_abundances.py nanosim_abundance.tsv *.isoforms.results rsem_abundance.tsv
 
 import sys
 
-# nanosim abundance file format
-# target_id est_counts tpm
-
 # rsem abundance file format
 # transcript_id gene_id length effective_length expected_count TPM FPKM IsoPct
-# The simulator only reads the TPM column.
+# Start from a learned *.isoforms.results file and only modify the TPM column.
+learned_abundance = sys.argv[2]
 
-fin = open(sys.argv[1], 'r')
-fout = open(sys.argv[2], 'w')
+def get_tpm_dict(nanosim_abundance):
+    # nanosim abundance file format
+    # target_id est_counts tpm
+    tpm_dict = {}
+    with open(nanosim_abundance, 'r') as fin:
+        next(fin)
+        for line in fin:
+            values = line.strip().split()
+            transcript_id = values[0]
+            tpm = values[2]
+            tpm_dict[transcript_id] = tpm
+    return tpm_dict
 
-next(fin)
-fout.write('transcript_id\tgene_id\tlength\teffective_length\texpected_count\tTPM\tFPKM\tIsoPct\n')
-for line in fin:
-    values = line.strip().split()
-    transcript_id = values[0]
-    tpm = values[2]
-    fout.write('{}\t*\t*\t*\t*\t{}\t*\t*\n'.format(transcript_id, tpm))
+tpm_dict = get_tpm_dict(sys.argv[1])
 
-fin.close()
+# tst_num = 0
+fout = open(sys.argv[3], 'w')
+with open(learned_abundance, 'r') as fin:
+    line = fin.readline()
+    fout.write(line)
+    for line in fin:
+        values = line.strip().split()
+        transcript_id = values[0]
+        if transcript_id in tpm_dict:
+            values[5] = tpm_dict[transcript_id]
+        else:
+            values[5] = '0.0000'
+            # tst_num += 1
+            # print(transcript_id)
+        fout.write('\t'.join(values) + '\n')
+# print(tst_num)
 fout.close()
