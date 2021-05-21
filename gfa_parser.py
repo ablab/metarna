c8b5344df48ef6bca8d91da17d31670635caf8bd
letovesnoi <lenk.ff@gmail.com>
2021-05-21 17:43:07 +0300

    Set small weights for links from gfa

--- gfa_parser.py
+++ gfa_parser.py
@@ -81,11 +81,11 @@ def gfa_to_G(gfa, kmer_size):
 
                 u, v, attr = line_to_edge(line)
                 G.add_edge(u, v, **attr)
-                nx.set_edge_attributes(G, {(u, v): graphs.get_weight_attr(cov[u], cov[v], 0, 0)})
+                nx.set_edge_attributes(G, {(u, v): graphs.get_weight_attr(cov[u], cov[v], 0.05, 0.05)})
 
                 u, v, attr = line_to_rc_edge(line)
                 G.add_edge(u, v, **attr)
-                nx.set_edge_attributes(G, {(u, v): graphs.get_weight_attr(cov[u], cov[v], 0, 0)})
+                nx.set_edge_attributes(G, {(u, v): graphs.get_weight_attr(cov[u], cov[v], 0.05, 0.05)})
     graphs.write_G_statistics(G.to_undirected())
     return G
 
